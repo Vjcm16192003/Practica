fdd4932905c9429b23be2acb987ff53df59f2d2a
Your Name <you@example.com>
2020-10-31 13:31:52 -0600

    SIUUUU

---
+++ Lab_examen_progra1/src/lab_examen_progra1/Lab_examen_progra1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_examen_progra1;
+
+/**
+ *
+ * @author Victor Jafet Cruz
+ */
+public class Lab_examen_progra1 {
+   public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Menu().setVisible(true);
+            }
+        });
+    }//fin main
+}//fin de la clase
+
